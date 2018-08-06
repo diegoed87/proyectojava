25627eacfc85b449270c71f00529ce22c14db2ad
CARLOS JAVIER PITA AMAYA <javierpita17@hotmail.com>
2018-08-06 14:58:52 -0500

    Cambio de tema a la interfza grafica
    Ventana.java
    TestVentana.java

--- Calculadora/src/presentation/Ventana.java
+++ Calculadora/src/presentation/Ventana.java
@@ -8,6 +8,10 @@ import java.awt.event.ActionListener;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -24,14 +28,32 @@ public class Ventana extends JFrame implements ActionListener{
 	private String[] nombreBotones= {"%","v¬","x2","1/x","CE","C","<-","/",
 			"7","8","9","X","4","5","6","-","1","2","3","+","+-","0",",","="};
 	private JButton[] botones = new JButton[24];
+	private JMenuBar barra;
+	private JMenu menuArchivo;
+	private JMenuItem itemSalir;
 	
 	public Ventana() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		setTitle("Calculadora");
 		setResizable(true);
 		setSize(270,350);
 		setLocationRelativeTo(null);
 		setLayout(new GridLayout(1, 1));
+		
+		addWindowListener(new java.awt.event.WindowAdapter() {
+			public void windowClosing(java.awt.event.WindowEvent evt) {
+				close();
+			}
+		});
+		
+		barra = new JMenuBar();
+		menuArchivo = new JMenu("Archivo");
+		itemSalir = new JMenuItem("Salir");
+		itemSalir.addActionListener(this);
+		
+		menuArchivo.add(itemSalir);
+		
+		barra.add(menuArchivo);
 		
 		panelPrincipal = new JPanel(new BorderLayout());
 		panelPrincipal.setBorder(BorderFactory.createTitledBorder("Calculadora"));
@@ -53,6 +75,7 @@ public class Ventana extends JFrame implements ActionListener{
 		panelPrincipal.add(panelTexto,BorderLayout.NORTH);
 		panelPrincipal.add(panelBotones,BorderLayout.CENTER);
 		
+		setJMenuBar(barra);
 		add(panelPrincipal);
 		
 	}
@@ -60,6 +83,16 @@ public class Ventana extends JFrame implements ActionListener{
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource()==botones[0]) {
 			System.out.println("Funciona!!");
+		}else
+			if(e.getSource()==itemSalir) {
+				close();
+			}
+	}
+	
+	public void close() {
+			if (JOptionPane.showConfirmDialog(this, "¿Desea realmente desea salir de la calculadora?", "Salir del sistema", JOptionPane.YES_NO_OPTION, JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
+			//JOptionPane.showC
+			System.exit(0);
 		}
 	}
 
--- Calculadora/src/test/TestVentana.java
+++ Calculadora/src/test/TestVentana.java
@@ -1,10 +1,17 @@
 package test;
 
+import java.awt.Color;
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import presentation.Ventana;
 
 public class TestVentana {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
+		UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+		UIManager.put("nimbusOrange", new Color(0, 170, 255));
 		Ventana v = new Ventana();
 		v.setVisible(true);
 	}
