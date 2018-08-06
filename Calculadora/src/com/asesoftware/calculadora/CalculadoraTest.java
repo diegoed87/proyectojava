f71069efb3bc1473c208d3ef494a82420c22fdc5
CARLOS JAVIER PITA AMAYA <javierpita17@hotmail.com>
2018-08-06 11:45:46 -0500

    Se quita dependencia JUnit

--- Calculadora/src/com/asesoftware/calculadora/CalculadoraTest.java
+++ Calculadora/src/com/asesoftware/calculadora/CalculadoraTest.java
@@ -1,84 +1,84 @@
 package com.asesoftware.calculadora;
 
-import static org.junit.Assert.*;
-
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+//import static org.junit.Assert.*;
+//
+//import org.junit.After;
+//import org.junit.AfterClass;
+//import org.junit.Before;
+//import org.junit.BeforeClass;
+//import org.junit.Test;
 
 public class CalculadoraTest {
 	
-	static Calculadora calc;
-	
-	/*
-	 * Inicializar
-	 */
-	@Before
-	public void before() {
-		System.out.println("Before()");
-		calc.clear();
-	}
-	
-	@BeforeClass
-	public static void beforeClass() {
-		System.out.println("BeforeClass()");
-		calc = new Calculadora();
-	}
-	
-	/*
-	 * Reinicio, limpiar, eliminar
-	 */
-	@After
-	public void after() {
-		System.out.println("After()");
-		calc.clear();
-	}
-	
-	@AfterClass
-	public static void afterClass() {
-		System.out.println("AfterClass()");
-	}
-	
-
-	@Test
-	public void testSumar() {
-		System.out.println("Sumar()");
-		int res = calc.sumar(2, 3);
-		int esperado = 5;
-		assertEquals("Suma no coincide",esperado, res);
-	}
-	
-	@Test
-	public void testResar() {
-		System.out.println("Restar()");
-		int res = calc.restar(3, 2);
-		int esperado = 1;
-		assertEquals(esperado, res);
-	}
-	
-	@Test
-	public void testDividir() {
-		System.out.println("Dividir()");
-		int res = calc.dividir(3, 2);
-		int esperado = 1;
-		assertEquals(esperado, res);
-	}
-	/*
-	 * Test puede recibir un parametro expected que busca la clase de la Excepcion
-	 */
-	@Test(expected=ArithmeticException.class)
-	public void testDividirCero() {
-		System.out.println("Dividir()");
-		int res = calc.dividir(3, 0);
-		int esperado = 1;
-		assertEquals(esperado, res);
-	}
-	
-	@Test(timeout=2000)
-	public void testMetodoOptimo() {
-		calc.metodoOptimo();
-	}
+//	static Calculadora calc;
+//	
+//	/*
+//	 * Inicializar
+//	 */
+//	@Before
+//	public void before() {
+//		System.out.println("Before()");
+//		calc.clear();
+//	}
+//	
+//	@BeforeClass
+//	public static void beforeClass() {
+//		System.out.println("BeforeClass()");
+//		calc = new Calculadora();
+//	}
+//	
+//	/*
+//	 * Reinicio, limpiar, eliminar
+//	 */
+//	@After
+//	public void after() {
+//		System.out.println("After()");
+//		calc.clear();
+//	}
+//	
+//	@AfterClass
+//	public static void afterClass() {
+//		System.out.println("AfterClass()");
+//	}
+//	
+//
+//	@Test
+//	public void testSumar() {
+//		System.out.println("Sumar()");
+//		int res = calc.sumar(2, 3);
+//		int esperado = 5;
+//		assertEquals("Suma no coincide",esperado, res);
+//	}
+//	
+//	@Test
+//	public void testResar() {
+//		System.out.println("Restar()");
+//		int res = calc.restar(3, 2);
+//		int esperado = 1;
+//		assertEquals(esperado, res);
+//	}
+//	
+//	@Test
+//	public void testDividir() {
+//		System.out.println("Dividir()");
+//		int res = calc.dividir(3, 2);
+//		int esperado = 1;
+//		assertEquals(esperado, res);
+//	}
+//	/*
+//	 * Test puede recibir un parametro expected que busca la clase de la Excepcion
+//	 */
+//	@Test(expected=ArithmeticException.class)
+//	public void testDividirCero() {
+//		System.out.println("Dividir()");
+//		int res = calc.dividir(3, 0);
+//		int esperado = 1;
+//		assertEquals(esperado, res);
+//	}
+//	
+//	@Test(timeout=2000)
+//	public void testMetodoOptimo() {
+//		calc.metodoOptimo();
+//	}
 
 }
